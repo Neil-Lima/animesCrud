f4501eee197338d25d4f599964c8e4922e03e0af
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2024-08-05 11:35:53 -0300

    j

--- backend/index.js
+++ backend/index.js
@@ -4,27 +4,26 @@ const cors = require('cors');
 const connectDB = require('./config/database');
 const animeRoutes = require('./routes/animeRoutes');
 
-
 const app = express();
 const PORT = process.env.PORT || 3334;
 
 connectDB();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://animescrud.netlify.app/', 'https://animes-crud.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://animescrud.netlify.app', 'https://animes-crud.vercel.app'],
   optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// Rota de boas-vindas
+// Welcome route
 app.get('/', (req, res) => {
-  res.json({ message: 'Bem-vindo à API de Animes!' });
+  res.json({ message: 'Welcome to the Anime API!' });
 });
 
 app.use('/api', animeRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
